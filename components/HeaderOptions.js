cd448eee14b0c2fcde760086594cb27388b46593
¨Kelly <fabianachara@gmail.com>
2022-09-10 17:27:28 -0500

    complete header section at search page

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,7 +1,44 @@
+import HeaderOptionSing from "../components/HeaderOptionSing"
+
+import {
+    DotsVerticalIcon,
+    MapIcon, 
+    PhotographIcon, 
+    PlayIcon,
+    SearchIcon,
+    NewspaperIcon
+
+} from '@heroicons/react/outline'
+
 function HeaderOptions() {
     return (
-        <div>
-            <h1>hi im the header options</h1>
+        <div className="flex w-full border-b
+         border-b-gray-200 
+         text-gray-700 
+         justify-around
+         text-sm 
+         lg:text-base
+         lg:justify-start">
+            <div className="flex lg:pl-52 space-x-6">
+<HeaderOptionSing Icon={SearchIcon} title="All"
+selected/>
+<HeaderOptionSing Icon={PhotographIcon} title="Images"
+/>
+<HeaderOptionSing Icon={MapIcon} title="Maps"
+/>
+<HeaderOptionSing Icon={PlayIcon} title="Videos"
+/>
+<HeaderOptionSing Icon={NewspaperIcon} title="News"
+/>
+<HeaderOptionSing Icon={DotsVerticalIcon} title="More"
+/>
+
+
+            </div>
+
+            <div className="flex ml-auto pr-2">
+                <p className="link">Tools</p>
+            </div>
         </div>
     );
 }
